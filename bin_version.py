8d64a2928e8fbf1ecf28639734ce648634a48e5a
Rendaw <spoo@zarbosoft.com>
2017-09-22 18:57:50 +0900

    Version name checking

--- bin_version.py
+++ bin_version.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 import argparse
 import subprocess
+import re
 
 if subprocess.call(['git', 'diff-index', '--quiet', 'HEAD']) != 0:
     raise RuntimeError('Working directory must be clean.')
 parser = argparse.ArgumentParser()
 parser.add_argument('version')
 args = parser.parse_args()
+if not re.match('\\d+\\.\\d+\\.\\d+', args.version):
+    args.error('version must be in the format N.N.N')
 subprocess.check_call([
     'mvn',
     'versions:set',
